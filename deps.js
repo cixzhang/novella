f850a78ac65a1c15235fbbd0314964a1da13932b
Cindy Zhang <cixzhang@gmail.com>
2017-10-29 13:41:42 -0700

    adding dep file

---
+++ deps.js
@@ -0,0 +1,19 @@
+
+import vue from 'vue';
+import markdown from 'markdown';
+import optionator from 'optionator';
+import naturalCompare from 'natural-compare';
+import rollup from 'rollup';
+import rollupVue from 'rollup-plugin-vue';
+import rollupJson from 'rollup-plugin-json';
+
+export default {
+  vue,
+  markdown,
+  optionator,
+  naturalCompare,
+  rollup,
+  rollupVue,
+  rollupJson,
+};
+
