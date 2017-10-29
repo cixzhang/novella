16f8479eee8e14a612c18cb9d4784bd9330a5209
Cindy Zhang <cixzhang@gmail.com>
2017-10-29 13:59:40 -0700

    deps building

--- deps.js
+++ deps.js
@@ -1,9 +1,9 @@
 
 import vue from 'vue';
-import markdown from 'markdown';
+import { markdown } from 'markdown';
 import optionator from 'optionator';
 import naturalCompare from 'natural-compare';
-import rollup from 'rollup';
+import { rollup } from 'rollup';
 import rollupVue from 'rollup-plugin-vue';
 import rollupJson from 'rollup-plugin-json';
 
